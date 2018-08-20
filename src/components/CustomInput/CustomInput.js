25b7d9867cfbdcbe9677cc88b58c332d93b785b8
Pascoal Gomes <pascoal@mfdc.biz>
2018-08-20 21:24:54 +1000

    changed the components structure to make the app more scalable

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,52 +1,41 @@
 import React from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
-// @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
-// @material-ui/icons
 import Clear from "@material-ui/icons/Clear";
 import Check from "@material-ui/icons/Check";
-// core components
+
 import customInputStyle from "assets/jss/material-dashboard-react/components/customInputStyle";
 
-function CustomInput({ ...props }) {
-	const {
-		classes,
-		formControlProps,
-		labelText,
-		id,
-		labelProps,
-		inputProps,
-		error,
-		success
-	} = props;
+const CustomInput = ({ classes, formControlProps, labelText, id, labelProps, inputProps, error, success }) => {
 
 	const labelClasses = classNames({
 		[" " + classes.labelRootError]: error,
 		[" " + classes.labelRootSuccess]: success && !error
 	});
+
 	const underlineClasses = classNames({
 		[classes.underlineError]: error,
 		[classes.underlineSuccess]: success && !error,
 		[classes.underline]: true
 	});
+
 	const marginTop = classNames({
 		[classes.marginTop]: labelText === undefined
 	});
+
 	return (
 		<FormControl
 			{...formControlProps}
-			className={formControlProps.className + " " + classes.formControl}
-		>
+			className={formControlProps.className + " " + classes.formControl} >
 			{labelText !== undefined ? (
 				<InputLabel
 					className={classes.labelRoot + labelClasses}
 					htmlFor={id}
-					{...labelProps}
-				>
+					{...labelProps} >
 					{labelText}
 				</InputLabel>
 			) : null}
@@ -66,7 +55,7 @@ function CustomInput({ ...props }) {
 			) : null}
 		</FormControl>
 	);
-}
+};
 
 CustomInput.propTypes = {
 	classes: PropTypes.object.isRequired,
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export { 
+	Card,
+	CardAvatar,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	CardIcon
+ } from './Card';
+
+export { Button } from './CustomButtons/CustomButtons';
+export { CustomInput } from './CustomInput/CustomInput';
+export { CustomTabs } from './CustomTabs/CustomTabs';
