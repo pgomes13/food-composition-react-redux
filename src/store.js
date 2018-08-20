d8b3e969cfbd14aa081b20795d026c5bbec8fec8
Pascoal Gomes <pascoal@mfdc.biz>
2018-08-20 18:57:52 +1000

    added the redux dev tool extension

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import Immutable from 'immutable';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from './reducers/rootReducer';
 
 export default function configureStore(initialState = Immutable.Map()) {
 	return createStore(
 		rootReducer,
-		applyMiddleware(thunk)
-		);
+		composeWithDevTools(
+			applyMiddleware(thunk)
+	));
 };
